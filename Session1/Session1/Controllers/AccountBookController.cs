68f3d25a8398aedb4508c81e8651c5e4701944a8
賈斯汀 <justin.chang.tw@outlook.com>
2016-08-01 02:16:55 +0800

    加入我的記帳本功能

---
+++ Session1/Session1/Controllers/AccountBookController.cs
@@ -0,0 +1,35 @@
+﻿using Session1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Session1.Controllers
+{
+    public class AccountBookController : Controller
+    {
+        AccountBookData accountbookdata = AccountBookData.GetInstance();
+        // GET: AccountBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(AccountBookViewModel pageData)
+        {
+            var maxIden = (accountbookdata.AccountBookDataList.Last()).iden+1;
+            pageData.iden = maxIden;
+            accountbookdata.AccountBookDataList.Add(pageData);
+
+            return View();
+        }
+
+
+        public ActionResult GetAccountBookList()
+        {
+            return View(accountbookdata.AccountBookDataList);
+        }
+    }
+}
---
+++ Session1/Session1/Models/AccountBookModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Session1.Models
+{
+    
+    public class AccountBookViewModel
+    {
+        [DisplayName("#")]
+        public int? iden { get; set;}
+
+        [DisplayName("類別")]
+        [Required(ErrorMessage = "請輸入類別")]
+        public string AccountType { get; set; }
+        [DisplayName("金額")]
+        [Required(ErrorMessage = "請輸入金額")]
+        public decimal Amount { get; set; }
+        [DisplayName("日期")]
+        [Required(ErrorMessage = "請輸入日期")]
+        public string AcountDate { get; set; }
+        [DisplayName("備註")]
+        [Required(ErrorMessage = "請輸入備註")]
+        public string Mome { get; set; } 
+    }
+
+    //public class AccountBookListViewModel
+    //{
+    //    public List<AccountBookViewModel> AccountBookDataList { get; set; }
+    //}
+
+
+    public class AccountBookData
+    {
+        public List<AccountBookViewModel>  AccountBookDataList {get;set;}
+
+        private static AccountBookData accountbookdata = null;
+
+        public static AccountBookData GetInstance()
+        {
+            if(accountbookdata==null)
+            {
+                accountbookdata = new AccountBookData();
+            }
+            return accountbookdata;
+        }
+
+
+        private AccountBookData()
+        {
+            AccountBookDataList = new List<AccountBookViewModel>()
+            {
+                new AccountBookViewModel {iden=1,AccountType="1",Amount=10000,AcountDate="2017-01-01",Mome="" },
+                new AccountBookViewModel {iden=2,AccountType="2",Amount=10000,AcountDate="2017-01-02",Mome="" },
+                new AccountBookViewModel {iden=3,AccountType="1",Amount=10000,AcountDate="2017-01-03",Mome="" },
+                new AccountBookViewModel {iden=4,AccountType="2",Amount=10000,AcountDate="2017-01-04",Mome="" },
+                new AccountBookViewModel {iden=5,AccountType="1",Amount=10000,AcountDate="2017-01-05",Mome="" },
+                new AccountBookViewModel {iden=6,AccountType="2",Amount=10000,AcountDate="2017-01-06",Mome="" },
+                new AccountBookViewModel {iden=7,AccountType="1",Amount=10000,AcountDate="2017-01-07",Mome="" },
+                new AccountBookViewModel {iden=8,AccountType="2",Amount=10000,AcountDate="2017-01-08",Mome="" },
+                new AccountBookViewModel {iden=9,AccountType="1",Amount=10000,AcountDate="2017-01-09",Mome="" },
+
+            };
+        }
+
+
+
+    }
+
+}
