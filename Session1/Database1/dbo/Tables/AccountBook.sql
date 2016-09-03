2d947dec91a40446884fbd07e8f38e1d52941b86
賈斯汀 <justin.chang.tw@outlook.com>
2016-09-04 01:31:38 +0800

    日期選擇器使用「My97」

---
+++ Session1/Database1/dbo/Tables/AccountBook.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AccountBook] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [Categoryyy] INT              NOT NULL,
+    [Amounttt]   INT              NOT NULL,
+    [Dateee]     DATETIME         NOT NULL,
+    [Remarkkk]   NVARCHAR (500)   NOT NULL,
+    CONSTRAINT [PK_AccountBook] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
