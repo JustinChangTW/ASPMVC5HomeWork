2c99f1767d4ec8eff47213162714170f6ea0bcfe
賈斯汀 <justin.chang.tw@outlook.com>
2016-09-12 02:12:01 +0800

    將IdentityDB改到SkillTreeDb

--- Session1/Session1/Models/IdentityModels.cs
+++ Session1/Session1/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Session1.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DbModel", throwIfV1Schema: false)
         {
         }
 
